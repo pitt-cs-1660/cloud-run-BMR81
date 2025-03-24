ab76873103ed86f41f905aa1a33cdf180a51f2a2
BMR81 <BMR81@pitt.edu>
2025-03-24 03:24:53 -0400

    Everything Seems to be Working!

--- cc_cloud_run/main.py
+++ cc_cloud_run/main.py
@@ -23,15 +23,26 @@ async def read_root(request: Request):
     # ====================================
 
     # stream all votes; count tabs / spaces votes, and get recent votes
+    votes = votes_collection.stream()
+    tabs_count = 0
+    spaces_count = 0
+    vote_data = []
+    for v in votes:
+        vote = v.to_dict()
+        vote_data.append(vote)
+        if vote["team"] == "SPACES":
+            spaces_count += 1
+        elif vote["team"] == "TABS":
+            tabs_count += 1
 
     # ====================================
     # ++++ STOP CODE ++++
     # ====================================
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "tabs_count": 0,
-        "spaces_count": 0,
-        "recent_votes": []
+        "tabs_count": tabs_count,
+        "spaces_count": spaces_count,
+        "recent_votes": vote_data
     })
 
 
@@ -45,7 +56,12 @@ async def create_vote(team: Annotated[str, Form()]):
     # ====================================
 
     # create a new vote document in firestore
-    return {"detail": "Not implemented yet!"}
+    votes_collection.add({
+        "team": team,
+        "time_cast": datetime.datetime.utcnow().isoformat()
+    })
+
+    return {"detail": "Vote casted successfully!"}
 
     # ====================================
     # ++++ STOP CODE ++++
